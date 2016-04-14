5594d7116901b9d9dbda6b2ee84e5f070576211e
cynthiawupore <cynthiawuqian@outlook.com>
2016-04-14 15:27:28 +0800

    李锦记 v2 index-0

---
+++ js/js0.js
@@ -0,0 +1,86 @@
+// main-left
+function map(){
+	$(".map").fadeIn(1000);
+}
+function point(){
+	$(".point").fadeIn(500);
+	$(".point").addClass('flash'); 
+}
+function line(){
+	$(".line").fadeIn(500);
+}
+function china(){
+	$(".china").fadeIn(500);
+}
+function flag(){
+	$(".flag").fadeIn(500);
+	$(".flag").addClass('bounceInDown');
+}
+function description(){
+	$(".description").fadeIn(500);
+	$(".description").addClass('rollIn');
+	
+}
+setTimeout(map,0)
+setTimeout(point,1000)
+setTimeout(line,1500)
+setTimeout(china,2000)
+setTimeout(flag,2500)
+setTimeout(description,3500)
+
+
+// right top section1
+function mrt1_title(){
+	$(".mrt1-title").fadeIn(500);
+}
+function mrt1_history(){
+	$(".mrt1-history").fadeIn(500);
+	$(".mrt1-history").addClass('pulse');
+}
+function mrt1_pointTop(){
+	$(".mrt1-pointTop").fadeIn(500);
+}
+function mrt1_pointBtm(){
+	$(".mrt1-pointBtm").fadeIn(500);
+}
+function mrt1_words(){
+	$(".mrt1-words").fadeIn(500);
+}
+setTimeout(mrt1_title,4000)
+setTimeout(mrt1_history,4500)
+setTimeout(mrt1_pointTop,5000)
+setTimeout(mrt1_pointBtm,5500)
+setTimeout(mrt1_words,6000)
+
+
+
+
+
+
+// right bottom section1
+function mrb_title(){
+	$(".mrb-title").fadeIn(100);
+	$(".mrb-title").addClass('bounceInLeft');
+}
+function main_right_btm1(){
+	$(".main-right-btm1-box").fadeIn(500);
+	$(".main-right-btm1-box").addClass('main-right-btm1'); //3s
+}
+setTimeout(mrb_title,6100)
+setTimeout(main_right_btm1,6600)
+
+
+// right bottom section2
+function mrb2_grids(){
+	$(".mrb2-grids").fadeIn(500);
+}
+function mrb2_lineBlue(){
+	$(".mrb2-lineBlue").fadeIn(500);
+}
+function mrb2_lineYellow(){
+	$(".mrb2-lineYellow").fadeIn(500);
+}
+setTimeout(mrb2_grids,9600)
+setTimeout(mrb2_lineBlue,10100)
+setTimeout(mrb2_lineYellow,10600)
+
